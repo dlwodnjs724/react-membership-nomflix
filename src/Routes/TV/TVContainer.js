deafef95d8c4b980f5e27763f4ad4abada98ca48
이재원 <dlwodnjs724@naver.com>
2020-05-16 19:44:47 +0900

    #5.3 TV Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TVPresenter from './TVPresenter';
+import { tvApi } from 'api';
 
 export default class extends React.Component {
   state = {
@@ -10,15 +11,42 @@ export default class extends React.Component {
     erorr: null,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find TV information",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
-    const { nowPlaying, upcoming, popular, loading, error } = this.state;
+    const { topRated, popular, airingToday, loading, error } = this.state;
     return (
       <TVPresenter
-        nowPlaying={nowPlaying}
-        upcoming={upcoming}
+        topRated={topRated}
         popular={popular}
-        error={error}
+        airingToday={airingToday}
         loading={loading}
+        error={error}
       />
     );
   }
